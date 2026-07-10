63642f4b36e3ba0a57f00e6e5e3fd6b0f3a64ffe
IsranjanSathiyaseelan <isranjansathiyaseelan@gmail.com>
2026-07-10 22:56:21 +0530

    completed auth

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,27 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  app.enableCors();
+
+  const configService = app.get(ConfigService);
+  const port = configService.getOrThrow<number>('PORT');
+
+  await app.listen(port);
+
+  console.log(`Server running on http://localhost:${port}`);
 }
+
 bootstrap();
